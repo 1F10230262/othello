46c1f6ce81acb91529d4ce3b766e40943fdc7851
1F10230262 <s1f102302629@iniad.org>
2023-07-16 17:36:30 +0900

    オセロぽいの

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,22 +7,50 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 1, 1, 2, 0, 0, 0],
+    [0, 0, 2, 2, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const onClick = (x: number, y: number) => {
-    console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    newBoard[y][x] = turnColor;
-    setBoard(newBoard);
-    if (turnColor === 1) {
-      setTurnColor(2);
-    } else {
-      setTurnColor(1);
+    console.log(x, y);
+    const directions = [
+      [0, -1],
+      [0, 1],
+      [-1, 0],
+      [1, 0],
+      [1, -1],
+      [1, 1],
+      [-1, -1],
+      [-1, 1],
+    ];
+    for (let m = 0; m < 8; m++) {
+      const dx = directions[m][0];
+      const dy = directions[m][1];
+      console.log(dx, dy);
+      if (
+        board[y + dy] !== undefined &&
+        board[x + dx] !== undefined &&
+        board[y + dy][x + dx] === 3 - turnColor
+      ) {
+        for (let i = 2; i < 8; i++) {
+          if (
+            board[y + i * dy] !== undefined &&
+            board[x + i * dx] !== undefined &&
+            board[y + i * dy][x + i * dx] === turnColor
+          ) {
+            for (let n = 0; n < i; n++) {
+              newBoard[y + n * dy][x + n * dx] = turnColor;
+            }
+            setTurnColor(3 - turnColor);
+            break;
+          }
+        }
+      }
     }
+    setBoard(newBoard);
   };
 
   return (
